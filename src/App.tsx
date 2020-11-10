e60afc56229ab53b6ed9ad258948f2fe3dc49c28
Rasmus Rimestad <rasmus.rimestad@storebrand.no>
2020-11-10 09:28:16 +0100

    Format code to look prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SimpleForm} from "./simpleForm/SimpleForm";
 import {FormWithValidation} from "./formWithValidation/FormWithValidation";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import styled from 'styled-components';
 import {FormWithAdvancedValidation} from "./formWithAdvancedValidation/FormWithAdvancedValidation";
@@ -10,25 +10,25 @@ function App() {
   return (
     <div className="App">
       <Page>
-      <Tabs>
-        <TabList>
-          <Tab>Simple form</Tab>
-          <Tab>Form with validation</Tab>
-          <Tab>Form with advanced validation</Tab>
-        </TabList>
+        <Tabs>
+          <TabList>
+            <Tab>Simple form</Tab>
+            <Tab>Form with validation</Tab>
+            <Tab>Form with advanced validation</Tab>
+          </TabList>
 
-        <TabPanel>
-          <SimpleForm />
-        </TabPanel>
-        <TabPanel>
-          <FormWithValidation />
-        </TabPanel>
-        <TabPanel>
-          <FormWithAdvancedValidation />
-        </TabPanel>
-      </Tabs>
+          <TabPanel>
+            <SimpleForm/>
+          </TabPanel>
+          <TabPanel>
+            <FormWithValidation/>
+          </TabPanel>
+          <TabPanel>
+            <FormWithAdvancedValidation/>
+          </TabPanel>
+        </Tabs>
 
-</Page>
+      </Page>
     </div>
   );
 }
--- src/formWithAdvancedValidation/validationSchema.ts
+++ src/formWithAdvancedValidation/validationSchema.ts
@@ -5,8 +5,8 @@ export const validationSchema = yup.object().shape<FormState>({
   firstName: yup.string().required('This field is required'),
   firstNameBackwards: yup.string().test({
     name: 'testIfValueIsReallyBackwards',
-    test: function() {
-      if(this.parent.firstName.split("").reverse().join("") !== this.parent.firstNameBackwards) {
+    test: function () {
+      if (this.parent.firstName.split("").reverse().join("") !== this.parent.firstNameBackwards) {
         return this.createError({
           path: 'firstNameBackwards',
           message: 'This string should be your first name backwards'
